a63758da5975de24a59fa1a235dadd9eee2409e1
Luc De Nardi <denardiluc@gmail.com>
2025-02-27 19:52:17 -0500

    worked on changeType, revealAnswer, and started on startAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+    const buttonUpdate = () => {
+        setAttempts((prevAtt) => prevAtt - 1),
+            setProgress((prevProgress) => !prevProgress);
+    };
+    return (
+        <div>
+            {attempts}
+            <Button onClick={buttonUpdate}>Start Quiz</Button>
+            <Button onClick={() => setProgress(!progress)}>Stop Quiz</Button>
+            <Button onClick={() => setAttempts(attempts + 1)}>Mulligan</Button>
+        </div>
+    );
 }
